5ac6fb2e9bd406d572a0bb994f93378d1ced449c
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-15 14:05:48 +0200

    Update 01-test.js

--- src/js/01-test.js
+++ src/js/01-test.js
@@ -15,3 +15,30 @@ console.log('Контекст виклику функції');
 // bookShelf.addAuthor('Tanith Lee');
 // console.log(bookShelf.getAuthors());
 // // ["Bernard Cornwell", "Robert Sheckley", "Tanith Lee"]
+
+/*
+ * Exercise
+ */
+
+const pizzaPalace = {
+  pizzas: ['Supercheese', 'Smoked', 'Four meats'],
+  // Change code below this line
+  checkPizza(pizzaName) {
+    return pizzas.includes(pizzaName);
+  },
+  order(pizzaName) {
+    const isPizzaAvailable = checkPizza(pizzaName);
+
+    if (!isPizzaAvailable) {
+      return `Sorry, there is no pizza named «${pizzaName}»`;
+    }
+
+    return `Order accepted, preparing «${pizzaName}» pizza`;
+  },
+  // Change code above this line
+};
+
+pizzaPalace.order('Smoked');
+pizzaPalace.order('Four meats');
+pizzaPalace.order('Big Mike');
+pizzaPalace.order('Viennese');
