bdc42ee2f9e4d9648db3bc00c4124857104ebd57
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-15 14:04:17 +0200

    Update 01-test.js

--- src/js/01-test.js
+++ src/js/01-test.js
@@ -1,17 +1,17 @@
 console.log('Контекст виклику функції');
 
-const bookShelf = {
-  authors: ['Bernard Cornwell', 'Robert Sheckley'],
-  getAuthors() {
-    return this.authors;
-  },
-  addAuthor(authorName) {
-    this.authors.push(authorName);
-  },
-};
+// const bookShelf = {
+//   authors: ['Bernard Cornwell', 'Robert Sheckley'],
+//   getAuthors() {
+//     return this.authors;
+//   },
+//   addAuthor(authorName) {
+//     this.authors.push(authorName);
+//   },
+// };
 
-console.log(bookShelf.getAuthors());
-// ["Bernard Cornwell", "Robert Sheckley"]
-bookShelf.addAuthor('Tanith Lee');
-console.log(bookShelf.getAuthors());
-// ["Bernard Cornwell", "Robert Sheckley", "Tanith Lee"]
+// console.log(bookShelf.getAuthors());
+// // ["Bernard Cornwell", "Robert Sheckley"]
+// bookShelf.addAuthor('Tanith Lee');
+// console.log(bookShelf.getAuthors());
+// // ["Bernard Cornwell", "Robert Sheckley", "Tanith Lee"]
