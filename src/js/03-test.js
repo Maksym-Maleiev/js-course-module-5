820bffd9b03abb63f1c359d6f025d71edfd5d221
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-18 13:53:23 +0200

    Update 03-test.js

--- src/js/03-test.js
+++ src/js/03-test.js
@@ -1,3 +1,28 @@
 /*
  * Exercise
  */
+
+const historyService = {
+  orders: [
+    { email: 'jacob@hotmail.com', dish: 'Burrito' },
+    { email: 'solomon@topmail.net', dish: 'Burger' },
+    { email: 'artemis@coldmail.net', dish: 'Pizza' },
+    { email: 'solomon@topmail.net', dish: 'Apple pie' },
+    { email: 'jacob@hotmail.com', dish: 'Taco' },
+  ],
+  // Change code below this line
+  getOrdersLog() {
+    return orders
+      .map(order => `email: ${order.email} dish: ${order.dish}`)
+      .join(' - ');
+  },
+  getEmails() {
+    const emails = orders.map(order => order.email);
+    const uniqueEmails = new Set(emails);
+    return [...uniqueEmails];
+  },
+  getOrdersByEmail(email) {
+    return orders.filter(order => order.email === email);
+  },
+  // Change code above this line
+};
