1270ff247c53ae3c348012d9eec36128dc73ee26
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-19 12:31:02 +0200

    Update 04-test.js

--- src/js/04-test.js
+++ src/js/04-test.js
@@ -9,7 +9,14 @@ console.log(dog);
 console.log(animal.isPrototypeOf(dog));
 // true
 
-console.log(dog.hasOwnProperty('name')); // true console.log(dog.name); // 'Mango' console.log(dog.hasOwnProperty("legs")); // false console.log(dog.legs); // 4
+console.log(dog.hasOwnProperty('name'));
+// true
+console.log(dog.name);
+// 'Mango'
+console.log(dog.hasOwnProperty('legs'));
+// false
+console.log(dog.legs);
+// 4
 
 /*
  * Exercise
