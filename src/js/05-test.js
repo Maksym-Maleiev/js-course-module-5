a40d4d0788e74e11e124cb3f1f0bcd74c7d298ed
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-19 12:41:26 +0200

    Update 05-test.js

--- src/js/05-test.js
+++ src/js/05-test.js
@@ -1,3 +1,22 @@
 /*
  * Exercise
  */
+
+const ancestor = {
+  name: 'Paul',
+  age: 83,
+  surname: 'Dawson',
+  heritage: 'Irish',
+};
+// Change code below this line
+
+const parent = {};
+parent.name = 'Stacey';
+parent.surname = 'Moore';
+parent.age = 54;
+
+const child = {};
+child.name = 'Jason';
+child.age = 27;
+
+// Change code above this line
