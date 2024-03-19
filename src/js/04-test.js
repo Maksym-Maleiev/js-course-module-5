0b202ded959d8979d6474b3e9242f00eef7743b2
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-19 12:35:33 +0200

    Update 04-test.js

--- src/js/04-test.js
+++ src/js/04-test.js
@@ -1,22 +1,22 @@
 console.log('Прототип об`єкту');
 
-const animal = { legs: 4 };
-const dog = Object.create(animal);
-dog.name = 'Mango';
+// const animal = { legs: 4 };
+// const dog = Object.create(animal);
+// dog.name = 'Mango';
 
-console.log(dog);
-// { name: 'Mango', __proto__: animal }
-console.log(animal.isPrototypeOf(dog));
-// true
+// console.log(dog);
+// // { name: 'Mango', __proto__: animal }
+// console.log(animal.isPrototypeOf(dog));
+// // true
 
-console.log(dog.hasOwnProperty('name'));
-// true
-console.log(dog.name);
-// 'Mango'
-console.log(dog.hasOwnProperty('legs'));
-// false
-console.log(dog.legs);
-// 4
+// console.log(dog.hasOwnProperty('name'));
+// // true
+// console.log(dog.name);
+// // 'Mango'
+// console.log(dog.hasOwnProperty('legs'));
+// // false
+// console.log(dog.legs);
+// // 4
 
 /*
  * Exercise
