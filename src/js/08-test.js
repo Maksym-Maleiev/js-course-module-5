9b13414a76349f7f518228b1459505175d9bb687
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-23 11:33:54 +0200

    Update 08-test.js

--- src/js/08-test.js
+++ src/js/08-test.js
@@ -1,3 +1,16 @@
 console.log('Об`єкт параметрів');
 
-class User { // Деструктуризуємо об'єкт constructor({ name, email }) { this.name = name; this.email = email; } } const mango = new User({ name: "Mango", email: "mango@mail.com", }); console.log(mango); // {name: "Mango", email: "mango@mail.com"} const poly = new User({name: "Poly", email: "poly@mail.com", }); console.log(poly); // { name: "Poly", email: "poly@mail.com"}
+class User {
+  // Деструктуризуємо об'єкт
+  constructor({ name, email }) {
+    this.name = name;
+    this.email = email;
+  }
+}
+const mango = new User({ name: 'Mango', email: 'mango@mail.com' });
+console.log(mango);
+// {name: "Mango", email: "mango@mail.com"}
+
+const poly = new User({ name: 'Poly', email: 'poly@mail.com' });
+console.log(poly);
+// { name: "Poly", email: "poly@mail.com"}
