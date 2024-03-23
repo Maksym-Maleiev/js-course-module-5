4e827b6cd7dd21526df5e04b8a06aa7d83dc6a46
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-23 11:37:51 +0200

    Update 08-test.js

--- src/js/08-test.js
+++ src/js/08-test.js
@@ -34,3 +34,6 @@ console.log(car1);
 
 const car2 = new Car({ brand: 'BMW', model: 'X5', price: 58900 });
 console.log(car2);
+
+const car3 = new Car({ brand: 'Nissan', model: 'Murano', price: 31700 });
+console.log(car3);
