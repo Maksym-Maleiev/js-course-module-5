ba9209ceff8ab035043fa58240714f4f4487aba5
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-23 11:24:20 +0200

    Update 07-test.js

--- src/js/07-test.js
+++ src/js/07-test.js
@@ -1,1 +1,3 @@
 console.log('Конструктор класу');
+
+class User { // Синтаксис оголошення методу класу constructor(name, email) { // Ініціалізація властивостей екземпляра this.name = name; this.email = email; } } const mango = new User("Mango", "mango@mail.com"); console.log(mango); // {name: 'Mango', email: 'mango@mail.com'} const poly = new User("Poly", "poly@mail.com"); console.log(poly); // { name: 'Poly', email: 'poly@mail.com'}
