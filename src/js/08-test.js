0820c2a7fd46610ba54ba220f3d724d6a453c180
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-23 11:35:45 +0200

    Update 08-test.js

--- src/js/08-test.js
+++ src/js/08-test.js
@@ -18,3 +18,13 @@ console.log(poly);
 /*
  * Exercise
  */
+
+class Car {
+  // Change code below this line
+  constructor(brand, model, price) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+  }
+  // Change code above this line
+}
