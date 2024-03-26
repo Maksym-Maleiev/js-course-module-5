db4dfc71f6aee26aa3b3c67ee3fc8bbb707e86f6
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-26 11:00:32 +0200

    Update 09-test.js

--- src/js/09-test.js
+++ src/js/09-test.js
@@ -1,3 +1,16 @@
 console.log('Методи класу');
 
-class User { constructor({ name, email }) { this.name = name; this.email = email; } // Метод getEmail getEmail() { return this.email; } // Метод changeEmail changeEmail(newEmail) { this.email = newEmail; } }
+class User {
+  constructor({ name, email }) {
+    this.name = name;
+    this.email = email;
+  }
+  // Метод getEmail
+  getEmail() {
+    return this.email;
+  }
+  // Метод changeEmail
+  changeEmail(newEmail) {
+    this.email = newEmail;
+  }
+}
