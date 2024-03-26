f96abe15308614daf106b073c6c6352036187800
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-26 11:00:46 +0200

    Update 09-test.js

--- src/js/09-test.js
+++ src/js/09-test.js
@@ -5,10 +5,12 @@ class User {
     this.name = name;
     this.email = email;
   }
+
   // Метод getEmail
   getEmail() {
     return this.email;
   }
+
   // Метод changeEmail
   changeEmail(newEmail) {
     this.email = newEmail;
