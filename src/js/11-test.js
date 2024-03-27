be0a35f0525744bc7593c9032334222ba9acfb17
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-27 18:12:23 +0200

    Update 11-test.js

--- src/js/11-test.js
+++ src/js/11-test.js
@@ -1,3 +1,13 @@
 /*
  * Exercise
  */
+
+// Change code above this line
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
