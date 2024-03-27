4d8735f0adc867a338baf6eba9b74ebd691d842b
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-27 18:15:56 +0200

    Update 11-test.js

--- src/js/11-test.js
+++ src/js/11-test.js
@@ -2,12 +2,26 @@
  * Exercise
  */
 
+class StringBuilder {
+  constructor(initialValue = '') {
+    this.value = initialValue;
+  }
+
+  getValue() {
+    return this.value;
+  }
+}
+
 // Change code above this line
 const builder = new StringBuilder('.');
-console.log(builder.getValue()); // "."
+console.log(builder.getValue());
+// "."
 builder.padStart('^');
-console.log(builder.getValue()); // "^."
+console.log(builder.getValue());
+// "^."
 builder.padEnd('^');
-console.log(builder.getValue()); // "^.^"
+console.log(builder.getValue());
+// "^.^"
 builder.padBoth('=');
-console.log(builder.getValue()); // "=^.^="
+console.log(builder.getValue());
+// "=^.^="
