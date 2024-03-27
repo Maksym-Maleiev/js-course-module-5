c1c9ea85e65c6da038df67d292dddc5a872add6f
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-27 18:40:23 +0200

    Update 12-test.js

--- src/js/12-test.js
+++ src/js/12-test.js
@@ -30,3 +30,15 @@ console.log(mango.#email);
 /*
  * Exercise
  */
+
+class Car {
+  // Change code below this line
+
+  constructor({ brand, model, price }) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+  }
+
+  // Change code above this line
+}
