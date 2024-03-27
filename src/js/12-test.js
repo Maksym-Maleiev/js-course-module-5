aee02db6b03b91131935d5557d7c3d5770f1ca06
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-27 18:40:51 +0200

    Update 12-test.js

--- src/js/12-test.js
+++ src/js/12-test.js
@@ -46,3 +46,5 @@ class Car {
 new Car({ brand: 'Audi', model: 'Q3', price: 36000 });
 
 new Car({ brand: 'bmw', model: 'X5', price: 58900 });
+
+new Car({ brand: 'Nissan', model: 'Murano', price: 31700 });
