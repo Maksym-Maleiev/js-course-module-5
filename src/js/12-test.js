5a34ddfbe6fdd6624703735bc59480e7e1e42b17
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-27 18:39:15 +0200

    Update 12-test.js

--- src/js/12-test.js
+++ src/js/12-test.js
@@ -1,3 +1,28 @@
 console.log('Приватні властивості');
 
-class User { // Необов'язкове оголошення громадських властивостей name; // Обов'язкове оголошення приватних властивостей #email; constructor({ name, email }) { this.name = name; this.#email = email; } getEmail() { return this.#email; } changeEmail(newEmail) { this.#email = newEmail; } } const mango = new User({ name: "Mango", email: "mango@mail.com", }); mango.changeEmail("mango@supermail.com"); console.log(mango.getEmail()); // mango@supermail.com console.log(mango.#email); // Буде помилка, це приватна властивість
+class User {
+  // Необов'язкове оголошення громадських властивостей
+  name;
+  // Обов'язкове оголошення приватних властивостей
+  #email;
+
+  constructor({ name, email }) {
+    this.name = name;
+    this.#email = email;
+  }
+
+  getEmail() {
+    return this.#email;
+  }
+
+  changeEmail(newEmail) {
+    this.#email = newEmail;
+  }
+}
+
+const mango = new User({ name: 'Mango', email: 'mango@mail.com' });
+mango.changeEmail('mango@supermail.com');
+console.log(mango.getEmail());
+// mango@supermail.com
+console.log(mango.#email);
+// Буде помилка, це приватна властивість
