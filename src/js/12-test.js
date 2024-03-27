e8ba6a1a86e0fd74d5f092ad63a2ba5d9ef143ac
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-27 18:45:37 +0200

    Update 12-test.js

--- src/js/12-test.js
+++ src/js/12-test.js
@@ -52,8 +52,8 @@ class Car {
   // Change code above this line
 }
 
-console.log(new Car({ brand: 'Audi', model: 'Q3', price: 36000 }));
+const car1 = new Car({ brand: 'Audi', model: 'Q3', price: 36000 });
 
-new Car({ brand: 'bmw', model: 'X5', price: 58900 });
+const car2 = new Car({ brand: 'bmw', model: 'X5', price: 58900 });
 
 new Car({ brand: 'Nissan', model: 'Murano', price: 31700 });
