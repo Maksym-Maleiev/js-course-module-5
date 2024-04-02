6fd1d24b539feb4385f273c8ff908e21ed643fd6
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-02 12:29:51 +0300

    Update 14-test.js

--- src/js/14-test.js
+++ src/js/14-test.js
@@ -31,10 +31,17 @@ class StringBuilder {
 
 // Change code above this line
 const builder = new StringBuilder('.');
-console.log(builder.getValue()); // "."
+console.log(builder.getValue());
+// "."
+
 builder.padStart('^');
-console.log(builder.getValue()); // "^."
+console.log(builder.getValue());
+// "^."
+
 builder.padEnd('^');
-console.log(builder.getValue()); // "^.^"
+console.log(builder.getValue());
+// "^.^"
+
 builder.padBoth('=');
-console.log(builder.getValue()); // "=^.^="
+console.log(builder.getValue());
+// "=^.^="
