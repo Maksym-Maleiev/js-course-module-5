52181278071be2cf325e78e02ccc271f7aed9532
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-02 12:33:01 +0300

    Update 15-test.js

--- src/js/15-test.js
+++ src/js/15-test.js
@@ -2,4 +2,20 @@
  * Геттери та сетери
  */
 
-class User { #email; constructor({ name, email }) { this.name = name; this.#email = email; } // Геттер email get email() { return this.#email; } // Сеттер email set email(newEmail) { this.#email = newEmail; } }
+class User {
+  #email;
+  constructor({ name, email }) {
+    this.name = name;
+    this.#email = email;
+  }
+
+  // Геттер email
+  get email() {
+    return this.#email;
+  }
+
+  // Сеттер email
+  set email(newEmail) {
+    this.#email = newEmail;
+  }
+}
