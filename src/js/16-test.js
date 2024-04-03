1d0aa2a977efd9fe8fc763de1f2d7318dba31f5c
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-03 09:52:24 +0300

    Update 16-test.js

--- src/js/16-test.js
+++ src/js/16-test.js
@@ -37,3 +37,33 @@ console.log(mango.role);
 /*
  * Exercise
  */
+
+class Car {
+  // Change code below this line
+  #price;
+
+  constructor({ price }) {
+    this.#price = price;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  set price(newPrice) {
+    this.#price = newPrice;
+  }
+  // Change code above this line
+}
+
+const audi = new Car({ price: 35000 });
+console.log(audi.price);
+// 35000
+
+audi.price = 49000;
+console.log(audi.price);
+// 49000
+
+audi.price = 51000;
+console.log(audi.price);
+// 49000
