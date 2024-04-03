0875b538abab6398593aa3bf4475db80cfc66b48
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-03 10:02:01 +0300

    Update 17-test.js

--- src/js/17-test.js
+++ src/js/17-test.js
@@ -21,3 +21,23 @@ const mango = new User({ email: 'mango@mail.com' });
 
 console.log(User.isEmailTaken('poly@mail.com'));
 console.log(User.isEmailTaken('mango@mail.com'));
+
+/*
+ * Exercise
+ */
+
+class Car {
+  static #MAX_PRICE = 50000;
+  // Change code below this line
+
+  // Change code above this line
+  constructor({ price }) {
+    this.price = price;
+  }
+}
+
+const audi = new Car({ price: 36000 });
+const bmw = new Car({ price: 64000 });
+
+console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
