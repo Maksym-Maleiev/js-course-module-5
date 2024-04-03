cf4608e9d5ca3d59b8fd0da5b76a850403fdb863
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-03 09:59:22 +0300

    Update 17-test.js

--- src/js/17-test.js
+++ src/js/17-test.js
@@ -1,3 +1,23 @@
 /*
  * Статичні методи
  */
+
+class User {
+  static #takenEmails = [];
+
+  static isEmailTaken(email) {
+    return User.#takenEmails.includes(email);
+  }
+
+  #email;
+
+  constructor({ email }) {
+    this.#email = email;
+    User.#takenEmails.push(email);
+  }
+}
+
+const mango = new User({ email: 'mango@mail.com' });
+
+console.log(User.isEmailTaken('poly@mail.com'));
+console.log(User.isEmailTaken('mango@mail.com'));
