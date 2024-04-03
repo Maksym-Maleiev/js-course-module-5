12fbd401c02de53c5d4c6eefbd5cc3b40706d63e
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-03 10:51:26 +0300

    Update 19-test.js

--- src/js/19-test.js
+++ src/js/19-test.js
@@ -31,3 +31,42 @@ console.log(editor);
 // {email: 'mango@mail.com', posts: []}
 console.log(editor.email);
 // 'mango@mail.com'
+
+/*
+ * Exercise
+ */
+
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   // Change code below this line
+
+//   static AccessLevel = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+
+//   // Change code above this line
+// }
+
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
