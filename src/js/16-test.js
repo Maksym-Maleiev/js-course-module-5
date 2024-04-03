8c512e2c73765bafdb6d3720e27910eb0186d9f9
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-03 09:49:03 +0300

    Update 16-test.js

--- src/js/16-test.js
+++ src/js/16-test.js
@@ -2,4 +2,26 @@
  * Статичні властивості
  */
 
-class User { // Оголошення та ініціалізація статичної властивості static Roles = { ADMIN: "admin", EDITOR: "editor", }; #email; # role; constructor({email, role}) {this.#email = email; this.#role = role; } get role() { return this.#role; } set role (newRole) {this.#role = newRole; } } const mango = new User({email: "mango@mail.com", role: User.Roles.ADMIN, }); console.log(mango.Roles); // undefined console.log(User.Roles); // { ADMIN: "admin", EDITOR: "editor"} console.log(mango.role); // "admin" mango.role = User.Roles.EDITOR; console.log(mango.role); // "editor"
+class User {
+  // Оголошення та ініціалізація статичної властивості
+  static Roles = { ADMIN: 'admin', EDITOR: 'editor' };
+
+  #email;
+  #role;
+
+  constructor({ email, role }) {
+    this.#email = email;
+    this.#role = role;
+  }
+
+  get role() {
+    return this.#role;
+  }
+
+  set role(newRole) {
+    this.#role = newRole;
+  }
+}
+
+const mango = new User({ email: 'mango@mail.com', role: User.Roles.ADMIN });
+console.log(mango.Roles); // undefined console.log(User.Roles); // { ADMIN: "admin", EDITOR: "editor"} console.log(mango.role); // "admin" mango.role = User.Roles.EDITOR; console.log(mango.role); // "editor"
