d9df76ecd3a9f082a23c1ab16cde8e078c7704da
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-03 10:48:01 +0300

    Update 19-test.js

--- src/js/19-test.js
+++ src/js/19-test.js
@@ -1,3 +1,31 @@
 /*
  * Конструктор дочірнього класу
  */
+
+class User {
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+
+class ContentEditor extends User {
+  constructor({ email, posts }) {
+    // Виклик конструктора батьківського класу User
+    super(email);
+    this.posts = posts;
+  }
+}
+
+const editor = new ContentEditor({ email: 'mango@mail.com', posts: [] });
+console.log(editor);
+// {email: 'mango@mail.com', posts: []}
+console.log(editor.email);
+// 'mango@mail.com'
